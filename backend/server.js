f52ee4a5f9a25bf6c2d69fc2511ed55fb452be68
gokulschoyi <gokulsangamitrachoyi@gmail.com>
2023-10-24 18:36:17 +1100

    cors fix for new netlify site - (feature/market-prediction)

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ if (config.schedulerFlag === 'true') {
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors({ origin: ['https://crypticsage.netlify.app', 'https://localhost:3001'], credentials: true }))
+app.use(cors({ origin: ['https://crypticsage.netlify.app', 'https://crypticsagev.netlify.app', 'https://localhost:3001'], credentials: true }))
 app.use(bodyParser.json());
 // app.use(logger)
 
